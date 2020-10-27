f576a63fd071adf77e92af574a7522a4395ef117
geo_sudo <petrovkazanksvu@mail.ru>
2020-10-27 12:43:44 +0000

    Install web

--- main.tf
+++ main.tf
@@ -2,10 +2,29 @@ provider "aws" {
   region = "us-west-2"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-01fee56b22f308154"
+  ami           = "ami-06e54d05255faf8f6"
   instance_type = "t2.micro"
+
+  user_data = <<-EOF
+             #!/bin/bash
+             echo "Hello, World" > index.html
+             nohup busybox httpd -f -p 8080 &
+             EOF
 
   tags = {
     Name = "terraform-example"
+  }
+  vpc_security_group_ids = [aws_security_group.instance.id]
+}
+
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
